5574240f00140b874ba121a3ee109d858753f97a
siddharth <yadavsiddharth2406@gmail.com>
2023-08-30 17:48:20 +0530

    removed a bug and made changes to UI

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -12,9 +15,9 @@ io.on('connection', (socket) => {
   // Listen for messages
   socket.on('chat message', (message) => {
     // Broadcast the message to all connected clients
-    io.emit('chat message', message);
+    socket.broadcast.emit('chat message', message);
   });
-
+ 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
